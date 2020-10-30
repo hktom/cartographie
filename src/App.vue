24ee02b35a7b18ad64678f439c62b452e3aaab91
gedeon <gedebamba3@gmail.com>
2020-10-30 13:23:36 +0100

    add maps

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Maps></Maps>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Maps from "./components/Maps.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Maps
   }
 };
 </script>

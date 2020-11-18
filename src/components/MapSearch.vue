011a7e59ac28d4b3ebce6c5b422666fcebbf425c
gedeon <gedebamba3@gmail.com>
2020-11-18 15:39:13 +0100

    design multiple search

--- src/components/MapSearch.vue
+++ src/components/MapSearch.vue
@@ -1,9 +1,21 @@
 <template>
     <div class="map-search active-cyan-4 mb-4">
-        <font-awesome-icon v-if="search== ''" class="ico-search" icon="search" />
-        <font-awesome-icon v-else class="ico-search ico-close" icon="times" @click="resetSearch()"/>
-        <input class="form-control input-search"  type="text" :placeholder="$t('search-text')" aria-label="Search" 
-            v-model="search">
+        <div class="container-search">
+            <div class="container-input position-relative">
+                <font-awesome-icon v-if="search== ''" class="ico-search" icon="search" />
+                <font-awesome-icon v-else class="ico-search ico-close" icon="times" @click="resetSearch()"/>
+                <input class="form-control input-search"  type="text" :placeholder="$t('search-text')" aria-label="Search" 
+                    v-model="search">
+            </div>
+            <div class="container-filtre">
+                <select class="custom-select">
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+            </div>
+        </div>
     </div>  
 </template>
 
@@ -33,7 +45,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+    input, select{
+        &:focus{
+            outline: none;
+            border: none;
+            box-shadow: none;
+        }
+    }
     .map-search{
         position: absolute;;
         z-index:5;
@@ -43,18 +61,32 @@ export default {
     }
     .input-search{
         padding-right: 45px;
-        box-shadow: 0px 0px 20px rgba(0,0,0, 0.2);
         border:none;
-        &:focus{
-            outline: none;
+        width: 100%;
+    }
+    .container-search{
+        box-shadow: 0px 0px 20px rgba(0,0,0, 0.2);
+        background-color: white;
+        border-radius: 30px;
+        overflow: hidden;
+        padding: 5px;
+        display: flex;
+    }
+    .container-input{
+        width: calc(100% - 250px);
+    }
+    .container-filtre{
+        width: 250px;
+        border-left: 1px solid #aaa ;
+        select{
             border: none;
         }
     }
     .ico-search{
         position:absolute;
         z-index:5;
-        right: 35px;
-        top: 22px;
+        right: 15px;
+        top: 12px;
     }
     .ico-close{
         cursor: pointer;

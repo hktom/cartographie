24b2635c878645abacef9a99df74b17891fd4797
thesynyembo <nyembothesy@gmail.com>
2020-11-25 10:51:17 +0100

    fixe opacite arrière sur la barre de recherche

--- src/components/MapSearch.vue
+++ src/components/MapSearch.vue
@@ -140,7 +140,7 @@ export default {
         width: 100%;
     }
     .container-search{
-        box-shadow: 0px 0px 20px rgba(0,0,0, 0.05);
+        box-shadow: 0px 0px 20px rgba(0,0,0, 0.2);
         background-color: white;
         border-radius: 30px;
         padding: 5px;
